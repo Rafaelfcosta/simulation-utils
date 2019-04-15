f2ed17fb7d6cd1162703e13d0b3b2357f6a4fcf7
Rafael <rafaelferreiracosta26@gmail.com>
2019-04-15 00:22:55 -0300

    all math done lol

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const math = require('./math')
 
-math.uniform();
-math.exponential();
-math.triangular();
-math.normal();
+math.uniform(50, 200, 10000);
+math.triangular(10, 100, 30, 10000);
+math.exponential(10, 20, 10000);
+math.normal(6, 10000);
--- math.js
+++ math.js
@@ -1,17 +1,54 @@
-function uniform(){
-    
+function uniform(a, b, qtd) {
+    let results = []
+    for (let i = 0; i < qtd; i++) {
+        results.push(Math.random() * (b - a))
+    }
+    return results
 }
 
-function triangular(){
-    
+function triangular(a, b, c, qtd) {
+    let results = []
+    for (let i = 0; i < qtd; i++) {
+        let rand = Math.random()
+        if (rand < ((c - a) / (b - a))) {
+            let v = a + Math.sqrt(rand * (c - a) * (b - a))
+            results.push(v)
+        } else {
+            let v = b - Math.sqrt((1 - rand) * (b - c) * (b - a))
+            results.push(v)
+        }
+    }
+    return results
 }
 
-function exponential(){
-    
+function exponential(limitInf, mean, qtd) {
+    let results = []
+    let alfa = 1.0 / (mean - limitInf)
+    for (let i = 0; i < qtd; i++) {
+        let rand = Math.random()
+        let v = limitInf - (1 / alfa) * Math.log(rand)
+        results.push(v)
+    }
+    return results
 }
 
-function normal(){
-    
+function normal(mean, qtd) {
+    let results = []
+    for (let i = 0; i < qtd; i++) {
+        let Z = calcZ(mean)
+        results.push(Z)
+    }
+    return results
+}
+
+function calcZ(mean) {
+    let delta = 2
+    let sum12 = 0
+    for (let i = 0; i < 12; i++) {
+        sum12 += Math.random()
+    }
+
+    return mean + delta * (sum12 - 6.0);
 }
 
 module.exports = {
